0c3186a237f884eaaf83d0b6715ad201483a7a20
Julius <juliusb2@illinois.edu>
2024-09-26 10:21:53 -0500

    extra credit first pass

--- mp2/extra.py
+++ mp2/extra.py
@@ -2,6 +2,8 @@
 If you finish this module, you can submit it for extra credit.
 '''
 import numpy as np
+import submitted
+import cmath
 
 def rate_conversion(highrate_signal, highrate, lowrate):
     '''
@@ -20,4 +22,23 @@ def rate_conversion(highrate_signal, highrate, lowrate):
     You'll want to use a complex-valued lowrate_signal during Fourier synthesis,
     then return its real part.
     '''
+
+    N_0T = len(highrate_signal)
+    T = highrate / lowrate
+    N_0 = N_0T / T
+    coefficients = submitted.fourier_analysis(highrate_signal,int((N_0-1)//2))
+    lowrate_signal = np.zeros(int(N_0))
+    for n,losample in enumerate(lowrate_signal):
+        toSum = 0 + 0j
+        for k in range(-int((N_0-1)/2),int((N_0-1)/2)):
+        # for k,X_k in enumerate(coefficients):
+            X_k = coefficients[k]
+            toSum += cmath.rect(X_k,2*cmath.pi*k*n/N_0)
+        lowrate_signal[n] = toSum.real
+
+    coefficients = np.array(list(reversed(coefficients)) + [0] + coefficients)
+    print(len(coefficients))
+    print(coefficients)
+
+    return lowrate_signal, coefficients
     raise RuntimeError("You need to write this!")
