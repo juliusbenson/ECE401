4e4daa911953aaaf104e0dab254dcea44555ea1f
Julius <juliusb2@illinois.edu>
2024-12-05 10:40:41 -0600

    fixed compile-time errors bells still incorrect (wrong phase?)

--- mp6/submitted.py
+++ mp6/submitted.py
@@ -7,6 +7,7 @@ def todo_spectrum(x):
     Output:
     spec (N) - magnitude spectrum at N frequencies between 0 and fs, not including fs.
     '''
+    print(f"Calling todo_spectrum with x={x}")
     k = np.fft.fft(x)
     return np.abs(k)
     raise RuntimeError('You need to write this!')
@@ -21,6 +22,7 @@ def todo_findpeak(spec, fs, flo, fhi):
     Output:
     fpeak (scalar) - frequency of the highest-magnitude spectral sample, in Hertz
     '''
+    print(f"Calling todo_findpeak with spec={spec}, fs={fs}, flo={flo}, fhi={fhi}")
     f = np.linspace(0,fs,len(spec),endpoint=False)
     subspec = spec[(flo<f) & (f<fhi)]
     arg = np.argmax(subspec)
@@ -36,8 +38,11 @@ def todo_zeros(freqs, fs):
     Output:
     r (2,nfreqs) - an array of complex zeros on the unit circle, in complex conjugate pairs
     '''
+    print(f"Calling todo_zeros with freqs={freqs}, fs={fs}")
     r_0k = np.exp(np.multiply(freqs,2j*np.pi/fs))
-    return r_0k,np.conjugate(r_0k)
+    z = np.array([(r,np.conjugate(r)) for r in r_0k])
+    for zi in z: assert zi[0] == np.conjugate(zi[1])
+    return z.T
     raise RuntimeError('You need to write this!')
 
 def todo_poles(r,BW,fs):
@@ -49,8 +54,27 @@ def todo_poles(r,BW,fs):
     Output:
     p (2,nfreqs) - an array of complex poles with bandwidth BW, in complex conjugate pairs
     '''
-    a = np.exp(-BW*np.pi/fs)
-    return np.multiply(a,r)
+    print(f"Calling todo_poles with r={r}, BW={BW}, fs={fs}")
+    ra = r
+    assert np.isscalar(BW) # do not handle arrays here
+    if r.ndim == 1:
+        ra = np.array([r])
+
+    (_,nfreqs) = ra.shape
+
+    for ri in np.array(ra).T:
+        if ri[0] != np.conjugate(ri[1]):
+            raise ValueError(f"Not a conjugate pair: {ri[0]}, {ri[1]}")
+    p = np.zeros_like(ra)
+    for i in range(nfreqs):
+        a = np.exp(-BW*np.pi/fs)
+        assert np.isscalar(a)
+        p[0][i] = a*ra[0][i]
+        p[1][i] = a*ra[1][i]
+
+    # p = np.multiply(a,r)
+    for pi in p.T: assert pi[0] == np.conjugate(pi[1])
+    return p
     raise RuntimeError('You need to write this!')
 
 def todo_coefficients(r):
@@ -60,11 +84,15 @@ def todo_coefficients(r):
     Output:
     b (3,nfreqs) - an array of second-order polynomial coefficients, one per complex root pair
     '''
-    (roots,conjs) = r
+    print(f"Calling todo_coefficients with r={r}")
+    for ri in np.array(r).T: assert ri[0] == np.conjugate(ri[1])
+    roots = np.array(r).T
+    print(roots)
     nfreqs = len(roots)
-    b = np.zeros((nfreqs,3))
-    for i,root in enumerate(roots):
-        conj = conjs[i]
+    b = np.zeros((nfreqs,3),dtype='complex')
+    for i,ri in enumerate(roots):
+        root = ri[0]
+        conj = ri[1]
         b[i] = [1,-root-conj,(-root)*(-conj)]
     return b.T
     raise RuntimeError('You need to write this!')
@@ -79,6 +107,7 @@ def todo_freqresponse(a, b, N, version='right'):
     omega (N) - frequencies linearly spaced between 0 and 2pi, not including 2pi.
     H (N) - B(e^{jw})/A(e^{jw}) evaluated at the frequencies in the vector omega.
     '''
+    print(f"Calling todo_freqresponse with a={a}, b={b}, N={N}")
     omega = np.linspace(0,2*np.pi,N,endpoint=False)
     H = np.ones(N,dtype='complex')
     for n,w in enumerate(omega):
@@ -99,6 +128,7 @@ def todo_filter(x, a, b):
       Assume that x[n]==0 for n<0.
       Do not generate samples of y[n] for n >= N.
     '''
+    print(f"Calling todo_filter with x={x}, a={a}, b={b}")
     N = len(x)
     y = np.zeros(N)
     for n in range(N):
@@ -123,8 +153,9 @@ def todo_bell_pole(sample_rate, frequency, decay_time):
     @return:
     p (complex 2-array) - poles in the Z plane
     '''
+    print(f"Calling todo_bell_pole with sample_rate={sample_rate}, frequency={frequency}, decay_time={decay_time}")
     BW = 1/np.multiply(np.pi,decay_time)
-    r = todo_zeros(frequency,sample_rate)
+    r = todo_zeros([frequency],sample_rate)
     p = todo_poles(r,BW,sample_rate)
     return p
     raise RuntimeError('You need to write this!')
@@ -145,15 +176,17 @@ def todo_bell_simple(sample_rate, frequency, decay_time, duration):
     Output:
     x (array of length sample_rate*duration) - impulse response of the resonator
     '''
-    (root,conj) = todo_bell_pole(sample_rate,frequency,decay_time)
-    a = todo_coefficients(([root],[conj]))
-    a = np.concat(a)
-    b = [1, 0, 0]
+    print(f"Calling todo_bell_simple with sample_rate={sample_rate}, frequency={frequency}, decay_time={decay_time}, duration={duration}")
     samples = int(duration*sample_rate)
     d = np.zeros(samples)
     for i in range(samples):
         d[i] = np.cos(660/sample_rate)
-    # d[0] = 1 # kronecker delta = discrete impulse
+
+
+    p = todo_bell_pole(sample_rate,frequency,decay_time)
+    a = todo_coefficients(p)
+    a = np.concat(a)
+    b = [1, 0, 0]
     x = todo_filter(d, a, b)
     return x
     raise RuntimeError('You need to write this!')
@@ -172,19 +205,36 @@ def todo_bell_multitone(sample_rate, frequencies, decay_times, duration):
     x (array of length sample_rate*duration) - impulse response of the bell.
       This is just the sum of the todo_bell_simple impulse responses.
     '''
-    p = todo_bell_pole(sample_rate,frequencies,decay_times)
-    a = todo_coefficients(p)
-    b = [1,0,0]
+    print(f"Calling todo_bell_multitone with sample_rate={sample_rate}, frequencies={frequencies}, decay_times={decay_times}, duration={duration}")
     samples = int(duration*sample_rate)
     d = np.zeros(samples)
-
     for i in range(samples):
         d[i] = np.cos(660/sample_rate)
 
     x = d
-    for k in range(len(frequencies)):
-        y = todo_filter(x, a[:,k], b)
-        x = y
+    for i in range(len(decay_times)):
+        p = todo_bell_pole(sample_rate,frequencies[i],decay_times[i])
+        a = todo_coefficients(p)
+        a = np.concat(a)
+        b = [1,0,0]
+        x = todo_filter(x,a,b)
+    return x
 
-    return y
+    # p = []
+    # for i,decay_time in enumerate(decay_times):
+    #     p.append(todo_bell_pole(sample_rate,frequencies[i],decay_time)) # TODO: you need to do this separately for each decay time
+    # a = todo_coefficients(p)
+    # b = [1,0,0]
+    # samples = int(duration*sample_rate)
+    # d = np.zeros(samples)
+
+    # for i in range(samples):
+    #     d[i] = np.cos(660/sample_rate)
+
+    # x = d
+    # for k in range(len(frequencies)):
+    #     y = todo_filter(x, a[:,k], b)
+    #     x = y
+
+    # return y
     raise RuntimeError('You need to write this!')
